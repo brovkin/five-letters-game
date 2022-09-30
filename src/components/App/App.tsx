80debd7b5fff9d8f9787b2551afac2a6de73dad5
brovkin <brovkin.on@gmail.com>
2022-09-30 15:36:28 +0300

    fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,7 +7,7 @@ import { useAppSelector } from '@app/hooks';
 import './App.scss';
 
 const App: FC = () => {
-  const searchWord = useAppSelector((state) => state.grid.searchWord);
+  // const searchWord = useAppSelector((state) => state.grid.searchWord);
   const winnerWords = useAppSelector((state) => state.grid.winnerWords);
 
   return (
@@ -16,7 +16,7 @@ const App: FC = () => {
         <Logo />
         <WinnerWords winnerWords={winnerWords} />
         <FilledLetters />
-        <div className="app__hidden-word">Загаданное слово: {searchWord}</div>
+        {/*<div className="app__hidden-word">Загаданное слово: {searchWord}</div>*/}
         <LetterGrid />
       </div>
     </div>
